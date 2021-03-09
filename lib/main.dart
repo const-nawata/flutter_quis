52adfe72701df79918c91a2e963d864579aacdf0
Kostiantyn <ckolenchenko@yukon.cv.ua>
2021-03-09 18:17:54 +0200

    Lesson 7 final

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-import 'file:///E:/src/apps/flutter_quis/lib/pages/home_page.dart';
+import 'package:flutter_quis/pages/home_page.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
-  return runApp(StartApp());
+  return runApp(MyApp());
 }
 
-class StartApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override // переопрдеделение методов. В данном случае "build".
 
   Widget build(BuildContext context) {
     return MaterialApp(
       // Основной виджет.
       title: 'Quiz App',
-      theme: ThemeData(primarySwatch: Colors.amber),
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        textTheme: TextTheme(
+          caption: TextStyle(
+            fontSize: 22.0,
+            color: Colors.white,
+          ),
+        ),
+        fontFamily: 'Georgia',
+      ),
+
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quis/models/Question.dart';
+import 'package:flutter_quis/widgets/answer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,16 +9,46 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final QuestionData data = QuestionData();
-  int countResult = 0;
-  int questionIndex = 0;
+  int _countResult = 0;
+  int _questionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Testing'),),
+      appBar: AppBar(title: Text('Testing',style: Theme.of(context).textTheme.caption,),),
       body: Container(
-        child: Center(
-          child: Text('Something'),
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          color: const Color(0xff2a375a),
+          image: DecorationImage(
+            image: AssetImage('assets/images/Bond0004.jpg'),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                  data.questions[_questionIndex].title,
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
+
+            ...data.questions[_questionIndex].answers.map(
+                (value) => Answer(title: value['answer'],)
+            ).toList(),
+
+
+            ElevatedButton(
+                onPressed: () =>  setState(() {
+                  (_questionIndex < 3) ? _questionIndex++ : _questionIndex = 0;
+                  return _questionIndex;
+                }),
+              child: Text('Next'),
+            )
+
+          ],
         ),
       ),
     );
