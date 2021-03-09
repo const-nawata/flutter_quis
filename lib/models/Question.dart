8420d35a0efc472e842a1b9a7d3a582b860342f6
Kostiantyn <ckolenchenko@yukon.cv.ua>
2021-03-09 16:10:41 +0200

    Intermid

---
+++ lib/models/Question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class Question{
+  final String title;
+  final List<Map> answers;
+
+  Question({
+    @required this.title,
+    @required this.answers,
+  });
+}
